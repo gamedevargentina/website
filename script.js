fce5a7422eccf4f4c378afc92fca84310babf5ef
GamedevArgentina <113245024+gamedevargentina@users.noreply.github.com>
2025-08-13 17:01:20 -0300

    Add files via upload
    subida inicial

---
+++ script.js
@@ -0,0 +1,5 @@
+// Espera a que el contenido del DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', () => {
+    // Eliminada toda la lógica de cambio de tema
+    // Ahora solo modo oscuro permanente
+});
